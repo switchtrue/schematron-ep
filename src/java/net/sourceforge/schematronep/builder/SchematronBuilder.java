c513d6a4236ce570405992e442eee62e312a39b1
snapbox <snapbox>
2007-05-19 20:40:49 +0000

    Major refactor to improve error handling, use the message template wrapper, caching mechanism, etc.

--- src/java/net/sourceforge/schematronep/builder/SchematronBuilder.java
+++ src/java/net/sourceforge/schematronep/builder/SchematronBuilder.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
@@ -23,11 +21,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.ErrorListener;
 import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
@@ -38,7 +33,6 @@ import net.sourceforge.schematronep.Log;
 import net.sourceforge.schematronep.SchematronPlugin;
 import net.sourceforge.schematronep.TemplatesManager;
 import net.sourceforge.schematronep.Utils;
-import net.sourceforge.schematronep.preferences.PreferenceKeys;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
@@ -57,8 +51,6 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.ProcessingInstruction;
 import org.xml.sax.SAXException;
 
-import com.sun.corba.se.internal.javax.rmi.CORBA.Util;
-
 public class SchematronBuilder extends IncrementalProjectBuilder
 {
 	/** The factory that will create the transformers to apply the XSLs */
@@ -73,11 +65,23 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 
 	public static final String PATH_SKELETON_1_5 = "net/sourceforge/schematronep/xsl/skeleton1-5.xsl";
 
+	public static final String PATH_SKELETON_ISO = "net/sourceforge/schematronep/xsl/iso_schematron_skeleton.xsl";
+
+	public static final String FILE_NAME_SKELETON = "iso_schematron_skeleton.xsl";
+
+	public static final String PATH_ECLIPSE_WRAPPER = "net/sourceforge/schematronep/xsl/schematron-message.xsl";
+
 	public static final String BUILDER_ID = "net.sourceforge.schematronep.schematronBuilder";
 
 	private static final String MARKER_TYPE = "net.sourceforge.schematronep.xmlProblem";
 
-	public static final String VALIDATION_DELIMITER = "|";
+	public static final String RECORD_IDENTIFIER = "@@@";
+
+	public static final String FIELD_DELIMITER = ":";
+
+	public static final String MESSAGE_TYPE_ASSERT = "ASSERT";
+
+	public static final String MESSAGE_TYPE_REPORT = "REPORT";
 
 	private Map xmlFileMap = new HashMap();
 
@@ -86,16 +90,18 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 	private URL schematronXslURL = null;
 
 	/**
-	 * Initialise the transformationFactory with a config so that we can turn
-	 * line numbering on which we need to allow the markers to be placed at the
-	 * correct location in the UI
-	 */
+     * Initialise the transformationFactory with a config so that we can turn
+     * line numbering on which we need to allow the markers to be placed at the
+     * correct location in the UI
+     */
 	public SchematronBuilder()
 	{
 		Configuration config = new Configuration();
 		config.setLineNumbering(true);
-		// Suppress the "Running an XSLT 1.0 stylesheet with an XSLT 2.0 processor" messages
+		// Suppress the "Running an XSLT 1.0 stylesheet with an XSLT 2.0
+        // processor" messages
 		config.setVersionWarning(false);
+
 		txFac = new TransformerFactoryImpl(config);
 		resolver = new CustomURIResolver(txFac.getURIResolver(), null);
 		txFac.setURIResolver(resolver);
@@ -104,16 +110,23 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 
 		ClassLoader loader = SchematronPlugin.getDefault().getDescriptor().getPluginClassLoader();
 
-		schematronXslURL = loader.getResource(PATH_SKELETON_1_5);
+		// schematronXslURL = loader.getResource(PATH_SKELETON_1_5);
+		// schematronXslURL = loader.getResource(PATH_SKELETON_ISO);
+		schematronXslURL = loader.getResource(PATH_ECLIPSE_WRAPPER);
+	}
+
+	public static String getSkeletonFileName()
+	{
+		return FILE_NAME_SKELETON;
 	}
 
 	class SampleDeltaVisitor implements IResourceDeltaVisitor
 	{
 		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.core.resources.IResourceDelta)
-		 */
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.core.resources.IResourceDelta)
+         */
 		public boolean visit(IResourceDelta delta) throws CoreException
 		{
 			IResource resource = delta.getResource();
@@ -165,11 +178,11 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 	}
 
 	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.internal.events.InternalBuilder#build(int,
-	 *      java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
-	 */
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.internal.events.InternalBuilder#build(int,
+     *      java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
+     */
 	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException
 	{
 		if (kind == FULL_BUILD)
@@ -210,6 +223,12 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 		return baos.toByteArray();
 	}
 
+	/**
+	 * 
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
 	private Set extractSchemasFromPIs(IFile file) throws Exception
 	{
 		Set schemaList = new HashSet();
@@ -225,6 +244,9 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 
 				// Pull out the href value
 				String d = pi.getData();
+				
+				// TODO Use a StringTokenizer to rip this apart
+				
 				String sch = d.substring(d.indexOf("=\"") + 2, d.lastIndexOf("\""));
 				Log.debug("Schema = " + sch);
 
@@ -245,11 +267,19 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 		return schemaList;
 	}
 
+	/**
+	 * Overloaded method to convert from File to IFile
+	 * @param file
+	 */
 	private void processXML(File file)
 	{
 		processXML(Utils.convert(file));
 	}
 
+	/**
+	 * 
+	 * @param file
+	 */
 	private void processXML(IFile file)
 	{
 		deleteMarkers(file);
@@ -261,12 +291,9 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 		{
 
 			Set assocSchematronFiles = extractSchemasFromPIs(file);
-
-			Iterator it = assocSchematronFiles.iterator();
-
-			xmlFileMap.remove(file.getLocation());
-
 			xmlFileMap.put(file.getLocation(), assocSchematronFiles);
+			
+			Iterator it = assocSchematronFiles.iterator();
 
 			while (it.hasNext())
 			{
@@ -274,35 +301,24 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 
 				File schemaFile = new File(schFileName);
 
-				InputStream is = null;
+				Templates validationTemplates = templatesMgr.getTemplates(schemaFile);
 
-				try
+				if (validationTemplates == null)
 				{
-					Templates validationTemplates = templatesMgr.getTemplates(schemaFile);
+					Log.debug("Templates don't exist for schema " + schFileName + ". Creating...");
+					// Get the schematron xsl templates
+					Templates schematronTemplates = templatesMgr.createTemplates(schematronXslURL);
 
-					if (validationTemplates == null)
-					{
-						Log.debug("Templates don't exist for schema " + schFileName + ". Creating...");
-						// Get the schematron xsl templates
-						Templates schematronTemplates = templatesMgr.createTemplates(schematronXslURL);
-
-						// Now take the schematronTemplates and apply the schema to create another
-						// templates object for validation
-						validationTemplates = templatesMgr.createTemplates(schematronTemplates, schemaFile);						
-					}
-
-					byte[] output = transformToByteArray(file, validationTemplates);
-
-					processErrors(file, output, schemaFile);
+					// Now take the schematronTemplates and apply the schema
+                    // to create another
+					// templates object for validation
+					validationTemplates = templatesMgr.createTemplates(schematronTemplates, schemaFile);
 				}
-				finally
-				{
-					if (is != null)
-					{
-						is.close();
-					}
-				}
-			}
+
+				byte[] output = transformToByteArray(file, validationTemplates);
+
+				processOutput(file, output, schemaFile);			
+			}				
 		}
 		catch (Exception e)
 		{
@@ -311,6 +327,10 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 		}
 	}
 
+	/**
+	 * 
+	 * @param file
+	 */
 	private void processSCH(IFile file)
 	{
 		deleteMarkers(file);
@@ -321,7 +341,8 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 
 		String location = file.getLocation().toString();
 
-		// Remove this sch from the cache so that it will be recompiled when next used
+		// Remove this sch from the cache so that it will be recompiled when
+        // next used
 		templatesMgr.removeTemplates(file);
 
 		// Get the location of this file and hunt through the
@@ -335,12 +356,16 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 			if (schSet.contains(location))
 			{
 				// Call processXML with the correct path
-				Log.debug("Need to verify " + key);
+				Log.debug("Processing " + key);
 				this.processXML(key.toFile());
 			}
 		}
 	}
 
+	/**
+	 * 
+	 * @param resource
+	 */
 	void buildXML(IResource resource)
 	{
 		// Don't want to check files in the compiled directory, e.g. bin
@@ -354,15 +379,20 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 			}
 			else if (file.getFileExtension().equals(EXT_SCH))
 			{
-				// The schema is being recompiled. Need to make sure that
-				// the XML that refers to it is recompiled too
-				// How do we set file dependencies?
 				processSCH(file);
 			}
 		}
 	}
 
-	private void processErrors(IFile file, byte[] bs, File schemaFile) throws IOException
+	/**
+	 * Process the output from the validation and report any reports or assertions
+	 * 
+	 * @param file the XML file that is currently being validated
+	 * @param bs the message output from the validation XSL in a byte array
+	 * @param schemaFile the Schematron schema file that the file is being validated against
+	 * @throws IOException
+	 */
+	private void processOutput(IFile file, byte[] bs, File schemaFile) throws IOException
 	{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(bs)));
 		String line = reader.readLine();
@@ -370,16 +400,16 @@ public class SchematronBuilder extends IncrementalProjectBuilder
 		while (line != null)
 		{
 			Log.debug("processErrors: Read line = " + line);
-			StringTokenizer st = new StringTokenizer(line, VALIDATION_DELIMITER);
-
-			while (st.hasMoreTokens())
+			if (line.startsWith(RECORD_IDENTIFIER))
 			{
-				String token = st.nextToken();
-				int lineNumber = Integer.parseInt(token.substring(0, token.indexOf(":")));
-				this.addMarker(file, token + " from schema " + schemaFile.getAbsolutePath(), lineNumber,
-								IMarker.SEVERITY_ERROR);
+				StringTokenizer st = new StringTokenizer(line.substring(RECORD_IDENTIFIER.length()), FIELD_DELIMITER);
+				int lineNumber = Integer.parseInt(st.nextToken());
+				String type = st.nextToken();
+				String message = line.substring(line.lastIndexOf(FIELD_DELIMITER) + 1);
+				int level = (MESSAGE_TYPE_ASSERT.equals(type) ? IMarker.SEVERITY_ERROR : IMarker.SEVERITY_INFO);
+				
+				addMarker(file, message + " (see schema " + schemaFile.getAbsolutePath() + ")", lineNumber, level);
 			}
-
 			line = reader.readLine();
 		}
 		reader.close();
